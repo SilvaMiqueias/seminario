c26ce1ba0b08f9e107484affa025c027db07b75c
Miquéias Silva <d.miqueias@academico.ifg.edu.br>
2024-07-05 12:13:53 -0300

    Criação da tabela aluno e configuração

---
+++ src/main/resources/db/migration/V1.1__create_table_aluno.sql
@@ -0,0 +1,6 @@
+create table aluno (
+                      id bigint not null,
+                      name varchar(255),
+                      document varchar(100),
+                      primary key (id)
+)
